e76eef8979c16c3d81769248302d068c9c216a2a
Melissa Bagwell <mhbagwell@mail.fhsu.edu>
2023-03-21 18:37:58 -0400

    changes to quotes update file

--- api/quotes/update.php
+++ api/quotes/update.php
@@ -6,17 +6,18 @@ header('Access-Control-Allow-Methods: PUT');
 header('Access-Control-Allow-Headers: Access-Control-Allow-Methods, Content-Type, Access-Control-Allow-Methods, Authorization, X-Requested-With');
 
 //required files
-require('../../config/Database.php');
-require('../../models/Quote.php');
+include_once '../../config/Database.php';
+include_once '../../models/Quote.php';
+include_once '../../functions/Function.php';
 
-//Database
+//Db connection
 $database = new Database();
 $db = $database->connect();
 
-//Instantiate Category object
+//create quote object
 $quote = new Quote($db);
 
-//Get raw posted data
+//Get input
 $data = json_decode(file_get_contents("php://input"));
 
 if(isset($data->id) && isset($data->quote) && isset($data->author_id) && isset($data->category_id)){
@@ -27,10 +28,13 @@ if(isset($data->id) && isset($data->quote) && isset($data->author_id) && isset($
     $quote->author_id = $data->author_id;
     $quote->category_id = $data->category_id;
 
-    //Update
-    if($quote->update()){
+    if(!isValid($quote->author_id, $quote)){
+        echo json_encode(array('message'=> 'author_id Not Found'));
+    }else if(!isValid($quote->category_id, $quote)){
+        echo json_encode(array('message'=> 'category_id Not Found'));
+    }else if ($quote->update()){
         echo json_encode(array('id'=>$quote->id,'quote'=>$quote->quote,'author_id'=>$quote->author_id,'category_id'=>$quote->category_id));
-    } else {
+    }else {
         echo json_encode(array('message' => 'No Quotes Found'));
     }
 
