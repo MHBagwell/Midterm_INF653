d9d8f46bda8f03c58ba007a6c0d6e4e0f806385f
Melissa Bagwell <mhbagwell@mail.fhsu.edu>
2023-03-22 03:11:23 -0400

    changes to delete/quotes

--- api/quotes/delete.php
+++ api/quotes/delete.php
@@ -5,8 +5,11 @@
     header('Access-Control-Allow-Methods: DELETE');
     header('Access-Control-Allow-Headers: Access-Control-Allow-Headers, Access-Control-Allow-Methods,Content-Type, Authorization, X-Requested-With');
     
+    //required files
     include_once '../../config/Database.php';
     include_once '../../models/Quote.php';
+    include_once '../../models/Author.php';
+    include_once '../../functions/Function.php';
   
     //DB connection
     $database = new Database();
@@ -20,9 +23,13 @@
    
     $quote->id = $data->id;
 
+    if(!isValid($data->author_id, new Author($db))){
+        echo json_encode(array('message'=> 'author_id Not Found'));
+        exit();
+    }
+
     //Delete
-    if(isset($data->id)) {
-        $quote->delete();
+    if($quote->delete()) {
         echo json_encode(array('id'=>$quote->id));
     } else {
         echo json_encode(array('message' => 'No Quotes Found'));
