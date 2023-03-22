a3c6779b324d90f9d00702afad689c83ceba020b
Melissa Bagwell <mhbagwell@mail.fhsu.edu>
2023-03-21 21:08:39 -0400

    changes

--- api/quotes/update.php
+++ api/quotes/update.php
@@ -40,10 +40,10 @@ if(isset($data->id) && isset($data->quote) && isset($data->author_id) && isset($
         exit();
     }
     
-    if(!$quote->update()){
-        echo json_encode(array('message' => 'No Quotes Found'));
-    }else{
+    if ($quote->update()){
         echo json_encode(array('id'=>$quote->id,'quote'=>$quote->quote,'author_id'=>$quote->author_id,'category_id'=>$quote->category_id));
+    }else{
+       echo json_encode(array('message' => 'No Quotes Found'));
     }
 
 }else{
