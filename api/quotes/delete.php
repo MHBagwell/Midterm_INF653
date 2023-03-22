df45e32e7baf4865a39e318c54d117f7055a2fb6
Melissa Bagwell <mhbagwell@mail.fhsu.edu>
2023-03-21 21:29:59 -0400

    changes

--- api/quotes/delete.php
+++ api/quotes/delete.php
@@ -21,10 +21,10 @@
     $quote->id = $data->id;
 
     //Delete
-    if($quote->delete()){
-        echo json_encode(array('id'=>$quote->id));
-    }else {
-        echo json_encode(array('message'=>'No Quotes Found'));
+    if($quote->delete()) {
+        echo json_encode(array("id"=>$quote->id));
+    } else {
+        echo json_encode(array('message' => 'No Quotes Found'));
     }
 
 
